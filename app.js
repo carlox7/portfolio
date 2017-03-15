1b9e3d5cabdfd795d885f51b75da7cc881ad90a3
Carlo Santos <carlox7@gmail.com>
2017-03-14 19:20:37 -0700

    added constructor for projects

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function Project (name, description,url){
+  this.name = name;
+  this.description = description;
+  this.url = url;
+
+};
+
+var salmonCookies = new Project('Salmon Cookies', 'Week two assignment of Code 201', 'www.salmon.cookies');
+console.log(salmonCookies);
---
+++ projects.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var carloProjects = [
+  {
+    name: 'About Me',
+    description: 'something',
+    url:   'http://www',
+},
+  {
+    name: 'Salmon Cookies',
+    description: 'something',
+    url:   'http://www',
+},
+  {
+    name: 'Busmall',
+    description: 'something',
+    url:   'http://www',
+  }
+];
