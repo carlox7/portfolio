1174f1370ac03dff347e1effe86cc33929d70a94
Carlo Santos <carlox7@gmail.com>
2017-03-17 11:21:14 -0700

    fixed projects appending to page for portfolio 2 assignment

--- projectview.js
+++ projectview.js
@@ -2,7 +2,7 @@ var projectView = {};
 
 projectView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
-    $('.projects').hide();
+    $('#projects').hide();
     $('#' + $(this).attr('about')).show();
       console.log(this);
   });
