f902107c2b5ef3f9022b8f8baa4bf3a97fe18b41
Carlo Santos <carlox7@gmail.com>
2017-03-17 11:38:26 -0700

    removed commented code

--- projectview.js
+++ projectview.js
@@ -4,9 +4,8 @@ projectView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
     $('#projects').hide();
     $('#' + $(this).attr('about')).show();
-      console.log(this);
+
   });
-  //$('.main-nav .tab:first').click();
 };
 
 projectView.handleMainNav();
