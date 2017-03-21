e8fd47e440f36a5d23924a486f39bc623cf97093
Carlo Santos <carlox7@gmail.com>
2017-03-21 11:53:24 -0700

    filled out project sections with real text, continuing to work on styling of page

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,27 +2,27 @@
 
 var carloProjects = [
   {
-    name: 'About Me',
+    name: 'About Me Website',
     projectImage: 'projectpics/aboutme.png',
-    description: '<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)</p>',
+    description: '<p>This was the first project of Code Fellows Code 201 class. In this project we had to make a website about ourselves. The requirements were to use our new found knowledge of html and implement them to create a webpage with text and pictures. The other requirement was to apply javascript to create a quiz that prompts users for responses. We had to account for user input as well as edge cases where the user might type in a response not related to the intended question. This was the first full website I created on my own.</p>',
     url:   'https://carlox7.github.io/About-Me/',
 },
   {
     name: 'Salmon Cookies',
     projectImage: 'projectpics/salmoncookies.png',
-    description: '<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)</p>',
+    description: '<p>This was the second project of the Code Fellows Code 201 class. This project had us further use and refine our html skills and had us work in higher css skills we had learned. The project had us help a client build a website for their new business called Salmon Cookies. We needed to create a front page that used a salmon logo provided by the client and it was up to us to style the page to make it appealing to potential customers and investors. The other requirement was for us to create a table that forcasted cookie sales and totals based on theoretical hourly and daily sales. This table was for investors to guage their interest in investing in a Salmon Cookies store. We also had to create a form for the investor in order for them to type in their own theoretical cookie sales and have that form append to the table for ease of comparison to other stores.</p>',
     url:   'https://carlox7.github.io/salmon-cookies/',
 },
   {
     name: 'Busmall',
     projectImage: 'projectpics/busmall.png',
-    description: '<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)</p>',
+    description: '<p>Busmall was the third project of Code Fellows Code 201 class. I was tasked with creating a customer survey for the client that would allow people taking the survey to click on items that they would be interested in buying. The client would then have the survey data to show which items had the most customer interest and would pick those items to publish in a catalog. The survey results are presented to the client in a bar chart showing which items had the most clicks. The survey results were also stored in local storage so that the data world persist to the next person taking the survey. This way the client would have all the data in one chart. Another requirement of the project was to display three random items at a time to the person taking the survey and the three random items could not be the same as the previous three items. Logic had to be created in javascript to perform this task and also append the images to the page instantly. </p>',
     url:   'https://carlox7.github.io/bus-mall/',
   },
   {
     name: 'Escape Code 201',
     projectImage: 'projectpics/escape201.png',
-    description: '<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)</p>',
+    description: '<p>Escape Code 201 was the final project of Code Fellows Code 201. This was a group project with three other classmates. We created a developer game that tested the knowledge of people with some skills in web development. We used all the skills we had learned to develop this site from concept. The various pages of the site tested skills using the chrome developer tools, math and coding quizes and a debugger page. For my portion of the site I developed a slot machine game. It displayed would output a different win message depending on which three matching pictures you recieved. The number of times clicked before winning was also tracked in order to tell the user how much money they spent before winning. This value was then stored into local memory for use in another stage. I won \'t give you the answers to the game, but if you would like to try it yourself please click on the link.</p>',
     url:   'https://michaeltreat.github.io/Escape-code-201/',
 },
 ];
