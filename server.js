f2cb535d5b7a5037bebf74ce69492e909a802128
Carlo Santos <carlox7@gmail.com>
2017-03-24 11:16:31 -0700

    added 404 page

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 'use strict';
 
 const express = require('express');
+
 const PORT = 4000;
 
 const app = express();
 
 
 app.use(express.static('./public'));
+
+app.use(function(request, response, next){
+  response.status(404).sendFile('404.html', {root: './public'});
+
+})
 
 app.listen(PORT, function () {
   console.log(`Your app is being served on localhost: ${PORT}`);
