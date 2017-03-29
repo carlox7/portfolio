aa7225b3a6dd81b8f197f20e85860aa2be65fb8e
W. Ely Paysinger <paysinger@gmail.com>
2017-03-29 10:51:59 -0700

    removed corpse code and fixed tab

--- public/scripts/view/projectview.js
+++ public/scripts/view/projectview.js
@@ -1,19 +1,10 @@
+'use strict';
+
 var projectView = {};
 var x;
-
-// projectView.handleMainNav = function () {
-//   $('.main-menu').on('click', '.tab', function() {
-//     var $dataContent = $(this).attr('data-content');
-//     $('.main-nav').hide();
-//     x = $(this);
-//     $('#' + $(this).attr('data-content')).show();
-//   });
-// }
 
 projectView.initiateIndexPage = function() {
   Project.all.forEach(function(a){
     $('#projects').append(a.toHtml())
   });
-
-//projectView.handleMainNav();
 };
